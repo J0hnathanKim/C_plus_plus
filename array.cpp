cd95f6ff09b8cd9cc2d953dd8384bd01acc93dff
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-08-23 23:09:43 +0900

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	int* arr = new int[N];
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr+N);
+	cout << arr[N - 1];
+  delete[] arr;
+}
