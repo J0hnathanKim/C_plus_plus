ea82694e39d3c7dfdd55c7d0c2b154e0660ce212
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-10-24 12:51:13 +0900

    Create dynamic_class_array_exm.cpp

---
+++ Mid_Term/ch4/dynamic_class_array_exm.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+class Circle {
+private:
+	int radius;
+public:
+	Circle() {
+		radius = 1;
+		
+	}
+	Circle(int r) {
+		radius = r;
+		
+	}
+	~Circle() {
+	}
+	double getArea() {
+		return radius * radius * 3.14;
+	}
+	void setRadius(int r) {
+		radius = r;
+	}
+};
+
+int main() {
+	int count = 0;
+	int num = 0;
+	int radius;
+	Circle* p;
+	cout << "생성하고자 하는 원의 개수? ";
+	cin >> num;
+	p = new Circle[num];
+	for (int i = 1; i <= num; i++) {
+		cout << "원" << i << ": ";
+		cin >> radius;
+		p[i - 1].setRadius(radius);
+	}
+
+	for (int i = 1; i <= num; i++) {
+		cout << p[i - 1].getArea() << " ";
+		if (p[i - 1].getArea() >= 100 && p[i - 1].getArea() <= 200) count++;
+	}
+
+	cout << "\n면적이 100에서 200 사이인 원의 개수는 " << count;
+}
