d3028fe243d758a49c1db04c6ff66a8eda190d72
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-11-19 12:21:36 +0900

    Create 5-4_class_ref.cpp

---
+++ Fianl_term/ch5/5-4_class_ref.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ using namespace std;
+ class Circle {
+ intradius;
+ public:
+ Circle() { radius = 1; }
+ Circle(int radius) { this->radius = radius; }
+ void setRadius(intradius) { this->radius = radius; }
+ double getArea() { return 3.14*radius*radius; }
+ };
+ intmain() {
+ Circle circle;
+ Circle &refc= circle; 
+ refc.setRadius(10);
+ cout<< refc.getArea() << " " << circle.getArea();
+ }
